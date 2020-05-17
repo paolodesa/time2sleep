50c6f05aafd3bb8601724933062508e15e4bd23b
xxfeffo <23389826+xxfeffo@users.noreply.github.com>
2020-05-17 18:27:58 +0200

    Update LightControllerService.py

--- Controller/LightControllerService.py
+++ Controller/LightControllerService.py
@@ -1,6 +1,4 @@
-# Alarm scheduler microservice
 include MyMQTT
-include time
 include json
 
 class LightControllerService():
@@ -10,9 +8,9 @@ class LightControllerService():
         self.light = False
 
     def notify(topic, payload):
-        data = json.load(payload)
-            if topic == main_topic + 'bed/motion_sensor':
-                self.sensor_motion = data['value']
+        if topic == main_topic + 'bed':
+            message = json.load(payload)
+            self.sensor_motion = message['data'][0]['value']
     
     def LightOn():
         msg = json.dumps({'light'=True})
@@ -24,10 +22,14 @@ class LightControllerService():
         self.client.myPublish(main_topic + '/actuators', msg)
         self.light = False
         
-if __name__=='__name__':
+if __name__=='__main__':
     
     # I expect all the config information to be global and always updated. They are: wake_time.
     # Should I also check the vibration sensor?
+    main_topic = 'Time2Sleep/bedroom1/'
+    broker_host = '127.0.0.1'
+    broker_port = '1883'
+    
     myLightController = LightControllerService(clientID = 'LightControllerService', broker_host, broker_port)
     myLightController.client.start()
     myLightController.client.mySubscribe(main_topic + '/bed/motion_sensor')
