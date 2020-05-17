de1c92477e0fb13ce1d3b9c2dd57c30c1187833d
paolodesa <paolo.desa96@gmail.com>
2020-05-17 19:45:01 +0200

    fix for light microservice

--- Controller/LightControllerService.py
+++ Controller/LightControllerService.py
@@ -1,42 +1,47 @@
-include MyMQTT
-include json
+from MyMQTT import *
+import json
+import time
 
 class LightControllerService():
-    def __init__(clientID, broker_host, broker_port):
+    def __init__(self, clientID, broker_host, broker_port):
         self.client = MyMQTT(clientID, broker_host, broker_port, self)
         self.sensor_motion = 0
         self.light = False
 
-    def notify(topic, payload):
-        if topic == main_topic + 'bed':
-            message = json.load(payload)
+    def notify(self, topic, payload):
+        if topic == main_topic + 'light':
+            message = json.loads(payload)
             self.sensor_motion = message['data'][0]['value']
     
-    def LightOn():
-        msg = json.dumps({'light'=True})
-        self.client.myPublish(main_topic + '/actuators', msg)
+    def LightOn(self):
+        msg = json.dumps({'light':True})
+        self.client.myPublish(main_topic + 'actuators', msg)
         self.light = True
     
-    def LightOff():
-        msg = json.dumps({'light' = False})
-        self.client.myPublish(main_topic + '/actuators', msg)
+    def LightOff(self):
+        msg = json.dumps({'light':False})
+        self.client.myPublish(main_topic + 'actuators', msg)
         self.light = False
         
 if __name__=='__main__':
     
     # I expect all the config information to be global and always updated. They are: wake_time.
     # Should I also check the vibration sensor?
-    main_topic = 'Time2Sleep/bedroom1/'
+    main_topic = 'Time2sleep/bedroom1/'
     broker_host = '127.0.0.1'
-    broker_port = '1883'
+    broker_port = 1883
     
-    myLightController = LightControllerService(clientID = 'LightControllerService', broker_host, broker_port)
+    myLightController = LightControllerService('LightControllerService', broker_host, broker_port)
     myLightController.client.start()
-    myLightController.client.mySubscribe(main_topic + '/bed/motion_sensor')
-    while(True)
-        while (myLightController.light == False):
-            if (myLightController.sensor_motion == True):
-                myLightController.LightOn()
-        while (myLightController.light == True):
-            if myLightController.sensor_motion == False:
-                myLightController.LightOff()
+    time.sleep(1)
+    myLightController.client.mySubscribe(main_topic + 'light')
+    try:
+        while(True):
+            while (myLightController.light == False):
+                if (myLightController.sensor_motion >= 50):
+                    myLightController.LightOn()
+            while (myLightController.light == True):
+                if myLightController.sensor_motion < 50:
+                    myLightController.LightOff()
+    except KeyboardInterrupt:
+        myLightController.client.stop()
