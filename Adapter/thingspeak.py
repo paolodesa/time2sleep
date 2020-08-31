0f4b84d292fc935dd5b12d620849d27ca8db440b
Paolo De Santis <paolo.desa96@gmail.com>
2020-08-31 15:55:29 +0200

    Fixed adapter

--- Adapter/thingspeak.py
+++ Adapter/thingspeak.py
@@ -15,26 +15,28 @@ class LocalSubscriber():
         self.client = MyMQTT(clientID, broker_host, broker_port, self)
     def notify(self, msg_topic, msg_payload):
         payload = json.loads(msg_payload)
-        data = payload['data']
-        for d in data:
-            if d['type'] == 'vibration':
-                vibration = str(d['value'])
-            elif d['type'] == 'noise':
-                noise = str(d['value'])
-            elif d['type'] == 'motion':
-                motion = str(d['value'])
+        if msg_topic == local_topic + '/vibration':
+            vibration = str(payload['value'])
+        elif msg_topic == local_topic + '/noise':
+            noise = str(payload['value'])
+        elif msg_topic == local_topic + '/motion':
+            motion = str(payload['value'])
+        elif msg_topic == local_topic + '/temperature':
+            temperature = str(payload['value'])
+        elif msg_topic == local_topic + '/humidity':
+            humidity = str(payload['value'])
         ts_topic = 'channels/1127842/publish/AAN3J152MUTN1YYX'
         ts_client = TSPublisher('ts', 'mqtt.thingspeak.com', 1883).client
         ts_client.start()
-        ts_payload = 'field1=' + vibration + '&field2=' + noise + '&field3=' + motion
+        ts_payload = 'field1=' + vibration + '&field2=' + noise + '&field3=' + motion + '&field4=' + temperature + '&field5=' + humidity
         ts_client.myPublish(ts_topic, ts_payload)
         ts_client.stop()
 
 
-local_topic = 'Time2sleep/bedroom1/bed'
+local_topic = 'Time2sleep/bedroom1/sensors'
 local_client = LocalSubscriber('local', '127.0.0.1', 1883).client
 local_client.start()
-local_client.mySubscribe(local_topic)
+local_client.mySubscribe(local_topic + '/#')
 while True:
     try:
         time.sleep(1)
