e493e7eadeec82d3c0fdbc7593569c18c92a168c
Federico Fabiani <s269124@studenti.polito.it>
2020-09-23 18:34:18 +0200

    start up script added

---
+++ start_up.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('python3 /raspberry/cp_web_server.py')
+os.system('python3 /raspberry/sensors_reader.py')
+os.system('python3 /raspberry/AlarmActuatorService.py')
+os.system('python3 /raspberry/LightActuatorService.py')
