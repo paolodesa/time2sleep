6561308cd34908524b64c28dba29a01bd1fa3c9f
Federico Fabiani <s269124@studenti.polito.it>
2020-09-23 18:42:13 +0200

    start up script fixed

--- start_up.py
+++ start_up.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('python3 /raspberry/cp_web_server.py')
-os.system('python3 /raspberry/sensors_reader.py')
-os.system('python3 /raspberry/AlarmActuatorService.py')
-os.system('python3 /raspberry/LightActuatorService.py')
+os.system('python3 ~/time2sleep/raspberry/cp_web_server.py')
+os.system('python3 ~/time2sleep/raspberry/sensors_reader.py')
+os.system('python3 ~/time2sleep/raspberry/AlarmActuatorService.py')
+os.system('python3 ~/time2sleep/raspberry/LightActuatorService.py')
