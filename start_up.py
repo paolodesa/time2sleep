8baf7b62f772818bc66b908f50996e90905e0769
Federico <23389826+federico-fabiani@users.noreply.github.com>
2020-11-02 12:07:01 +0100

    Delete start_up.py

--- start_up.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, "./raspberry")
-sys.path.insert(0, "./etc")
-exec(open("raspberry/cp_web_server.py").read())
-
-# os.system('python3 ~/time2sleep/raspberry/cp_web_server.py')
-# os.system('python3 ~/time2sleep/raspberry/sensors_reader.py')
-# os.system('python3 ~/time2sleep/raspberry/AlarmActuatorService.py')
-# os.system('python3 ~/time2sleep/raspberry/LightActuatorService.py')
